742d2c21d8f2ea1833848c1468744803d6698e7e
Christian Young <christian.d.young23@gmail.com>
2021-04-29 14:36:20 -0700

    initial ideas

--- solver.py
+++ solver.py
@@ -14,7 +14,45 @@ def solve(G):
         c: list of cities to remove
         k: list of edges to remove
     """
-    pass
+    num_nodes = nx.number_of_nodes(G)
+    num_edges = nx.number_of_edges(G)
+
+    k_constraint = min(15, num_edges)
+    c_constraint = 1
+
+    nodes_removed = 0
+    edges_removed = 0
+
+    while nodes_removed < c and edges_removed < k:
+        try:
+            shortest_st_path = nx.dijkstra_path(G,0,num_nodes-1)
+        except nx.NetworkXNoPath:
+            break
+    
+    # small graphs c = 1, not considering edges
+    list of nodes
+
+    while True:
+        get shortest path
+        construct subgraph
+        find min cut nodes to disconnect
+        if cardinality of min cut nodes <= c and original graph is not disconnected:
+            store min cut nodes
+        else:
+            store previous best
+            break
+    while True:
+        get shortest path
+        
+    
+    # process edges next
+    
+
+
+
+
+
+    
 
 
 # Here's an example of how to run your solver.
